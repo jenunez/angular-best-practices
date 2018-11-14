10a5a47e1f271ead6c82af645e3d7e2c8960c0ef
Jesus Nunez <jesus.nunez@incontact.com>
2018-11-14 11:26:09 -0400

    Using Immutability

--- src/app/services/user-repository.service.ts
+++ src/app/services/user-repository.service.ts
@@ -10,7 +10,7 @@ export class UserRepositoryService {
 
   saveUser(user): Observable<any> {
     user.classes = user.classes || [];
-    this.currentUser = user;
+    this.currentUser = Object.assign({}, user, {classes: user.classes || []});
 
     return Observable.empty().delay(1000);
   }
@@ -22,7 +22,7 @@ export class UserRepositoryService {
     if (this.currentUser.classes.includes[classId])
       return Observable.throw('Already enrolled');
 
-    this.currentUser.classes.push(classId);
+    this.currentUser = Object.assign({}, this.currentUser, {classes: this.currentUser.classes.concat([classId])});
 
     return Observable.empty().delay(1000);
   }
@@ -34,7 +34,7 @@ export class UserRepositoryService {
     if (!this.currentUser.classes.includes(classId))
       return Observable.throw('Not enrolled');
 
-    this.currentUser.classes = this.currentUser.classes.filter(c => c.classId !== classId);
+    this.currentUser.classes = Object.assign({}, this.currentUser, {classes: this.currentUser.classes.filter(c => c.classId !== classId)});
 
     return Observable.empty().delay(1000);
   }
